cf76ca14ee85426d420a5107794eac77baec4a90
Roozki <zawadzkirowan@gmail.com>
2022-09-27 02:30:49 -0700

    3 channel PWM read success

--- firmware/RC_control/RC_control.ino
+++ firmware/RC_control/RC_control.ino
@@ -2,43 +2,22 @@
 
 
 //#include "GY521.h"
-#include <ros.h>
+
 #include <Wire.h>
 #include <Servo.h>
-#include <std_msgs/Int16.h>
-#include <std_msgs/Float64.h>
-#include <geometry_msgs/TwistStamped.h>
-#include <AccelStepper.h>
-
-//ros stuff
-
-ros::NodeHandle nh;
-
-std_msgs::Int16 heart;
-std_msgs::Int16 IR1;
-std_msgs::Int16 IR2;
-std_msgs::Int16 IR3;
-std_msgs::Int16 IR4;
-std_msgs::Int16 IR5;
+//#include <AccelStepper.h>
 
 
+#define throttlePin 2
+#define aileronPin 3
+#define elevatorPin 18
 
+#define prop1pin 7
+#define prop2pin 8
 
+Servo prop1; 
+Servo prop2; 
 
-//geometry_msgs::TwistStamped imu;
-geometry_msgs::Twist imu;
-ros::Publisher heartbeat("heartbeat", &heart);
-ros::Publisher ir_1("ir_sensors/IR1", &IR1);
-ros::Publisher ir_2("ir_sensors/IR2", &IR2);
- ros::Publisher ir_3("ir_sensors/IR3", &IR3);
- ros::Publisher ir_4("ir_sensors/IR4", &IR4);
- ros::Publisher ir_5("ir_sensors/IR5", &IR5);
-ros::Publisher imu_pub("imu_data", &imu);
-
-//ros::Subscriber<motorIn1 std_msgs::Int16>;
-
-
-Servo servo1; 
 int counter;
 int sdrivepin = 7; //small drive m
 
@@ -46,49 +25,61 @@ int motor[] = {0, 1};
 int motorDirPin[] = {3,4};
 int motorPulPin[] = {8,9};
 
-int throttlePin = 19;
-int aileronPin = 20;
 
 
 
+
+
+volatile long throttleprev = 0;
+volatile long throttlecurrent = 0;
+volatile long throttlepulses = 0;
+volatile long throttletmp = 0;
 int throttle = 0;
+
+
+volatile long aileronprev = 0;
+volatile long aileroncurrent = 0;
+volatile long aileronpulses = 0;
+volatile long ailerontmp = 0;
 int aileron = 0;
 
-int throttlestart;
-int throttleee;
-const int imu_address = 0x69; //I2C address of imu
+volatile long elevatorprev = 0;
+volatile long elevatorcurrent = 0;
+volatile long elevatorpulses = 0;
+volatile long elevatortmp = 0;
+int elevator = 0;
+
+
+//control
+int throttle1 = 0;
+int throttle2 = 0;
+
+int throttle1min = 90;
+int throttle2min = 90;
+
+
+
+//const int imu_address = 0x69; //I2C address of imu
 //accelerometer
 
 
-//std_msgs::Int16 acc_x;
-//std_msgs::Int16 acc_y;
-//std_msgs::Int16 acc_z; //exactly 16 bits
-// std_msgs::Int16 gyro_x;
-// std_msgs::Int16 gyro_y;
-// std_msgs::Int16 gyro_z;
-std_msgs::Float64 temperature; //hot hot hot
 
-float acc_x;
-float acc_y;
-float acc_z;
-float gyro_x;
-float gyro_y;
-float gyro_z;
+// float acc_x;
+// float acc_y;
+// float acc_z;
+// float gyro_x;
+// float gyro_y;
+// float gyro_z;
 
 
 
 
 
-char tmp_str[7];
-//int IR1;
-char* convert_int16_to_str(int16_t i) {
- sprintf(tmp_str, "%6d", i);
-   return tmp_str;
-}
+
 
 //motor control
 
-AccelStepper mot1;
+//AccelStepper mot1;
 //
 // void driveMotor(int motor, int dir, int speed){
 // int tmp = speed / 2;
@@ -106,84 +97,67 @@ AccelStepper mot1;
 
 
 void setup() {
-  // put your setup code here, to run once:
- counter ++;
- nh.initNode();
- nh.advertise(heartbeat);
-
-  nh.advertise(ir_1);
-   nh.advertise(ir_2);
-  nh.advertise(ir_3);
-  nh.advertise(ir_4);
-   nh.advertise(ir_5);
-
-  nh.advertise(imu_pub);
-    //pinMode(throttlePin, INPUT_PULLUP);
-
-// attachInterrupt(throttlePin, throttleUp, HIGH);
-// attachInterrupt(throttlePin, throttleDown, LOW);
+  Serial.begin(9600);
+  pinMode(throttlePin, INPUT_PULLUP);
+   pinMode(aileronPin, INPUT_PULLUP);
+    pinMode(elevatorPin, INPUT_PULLUP);
+ attachInterrupt(digitalPinToInterrupt(throttlePin), throttleCHECK, CHANGE);
+ attachInterrupt(digitalPinToInterrupt(aileronPin), aileronCHECK, CHANGE);
+ attachInterrupt(digitalPinToInterrupt(elevatorPin), elevatorCHECK, CHANGE);
 
 
-  Serial.begin(57600);
-  Wire.begin();
-  Wire.beginTransmission(imu_address);
-  Wire.write(0x6B); //PWR_MGMT_1 register
-  Wire.write(0); //wakey wakey MPU-6050
-  Wire.endTransmission(true);
-//servo1.attach(7);
-    nh.spinOnce();
 
+// Wire.begin();
+  // Wire.beginTransmission(imu_address);
+  // Wire.write(0x6B); //PWR_MGMT_1 register
+  // Wire.write(0); //wakey wakey MPU-6050
+  // Wire.endTransmission(true);
+prop1.attach(prop2pin);
+prop2.attach(prop2pin);
+   
 
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
 }
 
 void loop() {   
-  IR1.data = analogRead(A0);
-  IR2.data = analogRead(A1);
-  IR3.data = analogRead(A2);
-  IR4.data = analogRead(A3);
-  IR5.data = analogRead(A4);
-  if(Serial.available() > 0){
-  //  int tmp = Serial.read();
-//analogWrite(8, tmp);
-Serial.println(Serial.read());
-
-  }
-  //Serial.println("tmp");
-
-  //throttle = digitalRead(throttlePin);
- //aileron = analogRead(aileronPin);
+  
+  
+//Serial.println("loopin");
 
 
-  //Serial.print("throttle: "); Serial.println(throttle);
-   // Serial.print("aileron: "); Serial.println(aileron);
-//counter ++;
-//if (counter >= 100){
-   // heart.data ++;
 
- // heartbeat.publish( &heart);
+if (throttletmp < 2000){
+  throttle = throttletmp;
+}
+if (ailerontmp < 2000){
+  aileron = ailerontmp;
+}
+if (elevatortmp < 2000){
+  elevator = elevatortmp;
+}
+  Serial.print("throttle:         ");   Serial.print(throttle);   Serial.print("     aileron:   ");   Serial.print(aileron);   Serial.print("     elevator:  ");   Serial.println(elevator);
 
-//counter = 0;
-//}
-ir_1.publish( &IR1);
- ir_2.publish( &IR1);
- //ir_3.publish( &throttle);
- //ir_4.publish( &aileron);
-// ir_5.publish( &IR1);
+
+throttle1 = throttle;
+throttle2 = throttle;
+
+prop1.write(map(throttle1, 1040, 2000, throttle1min, 180));
+prop2.write(map(throttle2, 1040, 2000, throttle2min, 0));
 
 
 
 
 
 
-delay(10);
+
+
+
 
 //   Wire.beginTransmission(imu_address);
 //   Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H) [MPU-6000 and MPU-6050 Register Map and Descriptions Revision 4.2, p.40]
 //  Wire.endTransmission(false); // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
 //   Wire.requestFrom(imu_address, 7*2, true); // request a total of 7*2=14 registers
   
-  nh.spinOnce();
 
 //"Wire.read()<<8 | Wire.read();" means two registers are read and stored in the same variable
   // acc_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
@@ -206,47 +180,59 @@ delay(10);
   // Serial.print(" | gZ = "); Serial.print(convert_int16_to_str(gyro_z));
   //  Serial.println();
   //Serial.println(IR1.data);
+
+
+
+
   
-//packing up the accelerometer data in ROS msgs
-
-  // imu.twist.linear.x = acc_x;
-  //  imu.twist.linear.x = acc_y;
-  //   imu.twist.linear.x = acc_z;
-  //    imu.twist.angular.x = gyro_x;
-  //    imu.twist.angular.y = gyro_y;
-  //    imu.twist.angular.z = gyro_z;
-  imu.linear.x = acc_x;
-   imu.linear.x = acc_y;
-    imu.linear.x = acc_z;
-     imu.angular.x = gyro_x;
-     imu.angular.y = gyro_y;
-     imu.angular.z = gyro_z;
-
-
-imu_pub.publish( &imu);
-
-
-
-  // delay
-  delay(10);
-  //counter +1;
 //servo1.write(map(acc_x, -25000, 25000, 20, 150));
 
 
-  nh.spinOnce();
 }
 
-// void throttleUp(){
-// throttlestart = micros();
+void throttleCHECK(){
+throttlecurrent = micros();
+if (throttlecurrent > throttleprev){
+  throttletmp = throttlecurrent - throttleprev;
+  throttleprev = throttlecurrent;
+  
+}
 
 
-// }
+}
 
-// void throttleDown(){
-//   throttle = micros() - throttlestart;
+void aileronCHECK(){
+ 
+  aileroncurrent = micros();
+if (aileroncurrent > aileronprev){
+  ailerontmp = aileroncurrent - aileronprev;
+  aileronprev = aileroncurrent;
+  
+}
+
+}
+
+void elevatorCHECK(){
+ 
+  elevatorcurrent = micros();
+if (elevatorcurrent > elevatorprev){
+  elevatortmp = elevatorcurrent - elevatorprev;
+  elevatorprev = elevatorcurrent;
+  
+}
+
+}
 
 
-// }
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ firmware/cursed_230_nerfed/cursed_230_nerfed.ino
@@ -0,0 +1,246 @@
+
+
+//libraries, some are not used but I will need them later
+//#include "GY521.h"
+#include <ros.h> //I learned ROS over the summer
+#include <Wire.h>
+#include <Servo.h>
+#include <std_msgs/Int16.h>
+//#include <std_msgs/Float64.h>
+#include <geometry_msgs/TwistStamped.h>
+
+
+
+//ros stuff
+
+ros::NodeHandle nh;
+//these are ros ready messages that contain Ints 
+std_msgs::Int16 heart; //heartbeat for debugging purposes
+std_msgs::Int16 IR1;
+std_msgs::Int16 IR2;
+//std_msgs::Int16 IR3;
+//std_msgs::Int16 IR4;
+//std_msgs::Int16 IR5;
+
+
+
+//arduino uno is very slow, so I must be carefull what data im sending
+//This sets up ROS publishers and 
+
+//geometry_msgs::TwistStamped imu;
+//geometry_msgs::Twist imu;
+ros::Publisher heartbeat("heartbeat", &heart);
+ros::Publisher ir_1("ir_sensors/IR1", &IR1);
+ros::Publisher ir_2("ir_sensors/IR2", &IR2);
+// ros::Publisher ir_3("ir_sensors/IR3", &IR3);
+// ros::Publisher ir_4("ir_sensors/IR4", &IR4);
+// ros::Publisher ir_5("ir_sensors/IR5", &IR5);
+
+//ros::Publisher imu_pub("imu_data", &imu);
+
+Servo servo1; 
+int counter;
+int sdrivepin = 7; //small drive m
+
+const int imu_address = 0x68; //I2C address of imu
+//accelerometer
+
+
+//std_msgs::Int16 acc_x;
+//std_msgs::Int16 acc_y;
+//std_msgs::Int16 acc_z; //exactly 16 bits
+// std_msgs::Int16 gyro_x;
+// std_msgs::Int16 gyro_y;
+// std_msgs::Int16 gyro_z;
+//std_msgs::Int16 temperature; //hot hot hot
+
+// float acc_x;
+// float acc_y;
+// float acc_z;
+// float gyro_x;
+// float gyro_y;
+// float gyro_z;
+
+
+char tmp_str[7];
+//int IR1;
+char* convert_int16_to_str(int16_t i) {
+ sprintf(tmp_str, "%6d", i);
+   return tmp_str;
+}
+
+//float mapFloat(std_msgs::Float64 input, std_msgs::Float64 min, std_msgs::Float64 max, std_msgs::Float64 tomin, std_msgs::Float64 tomax );
+
+void setup() {
+  // put your setup code here, to run once:
+ counter ++;
+ //im happy to explain my ROS integration in person. Im planning to use it to visualize data for debugging.
+ nh.initNode();
+ nh.advertise(heartbeat);
+
+  nh.advertise(ir_1);
+   nh.advertise(ir_2);
+  // nh.advertise(ir_3);
+  // nh.advertise(ir_4);
+  // nh.advertise(ir_5);
+
+  //nh.advertise(imu_pub);
+
+  Serial.begin(9600);
+  // Wire.begin();
+  // Wire.beginTransmission(imu_address);
+  // Wire.write(0x6B); //PWR_MGMT_1 register
+  // Wire.write(0); //wakey wakey MPU-6050
+  // Wire.endTransmission(true);
+//servo1.attach(7);
+    nh.spinOnce();
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+}
+
+void loop() {   
+  IR1.data = analogRead(A0);
+//counter ++;
+//if (counter >= 100){
+   // heart.data ++;
+
+ // heartbeat.publish( &heart);
+
+//counter = 0;
+//}
+
+
+
+
+//IR1.data = mapFloat(IR1.data, 0.0, 1023.0, 0.0, 5.0); //map(IR1.data, 0, 1023, 0, 500);
+ir_1.publish( &IR1);
+// ir_2.publish( &IR1);
+// ir_3.publish( &IR1);
+// ir_4.publish( &IR1);
+// ir_5.publish( &IR1);
+
+
+
+
+
+
+
+//delay(10);
+
+//   Wire.beginTransmission(imu_address);
+//   Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H) [MPU-6000 and MPU-6050 Register Map and Descriptions Revision 4.2, p.40]
+//  Wire.endTransmission(false); // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
+//   Wire.requestFrom(imu_address, 7*2, true); // request a total of 7*2=14 registers
+  
+  nh.spinOnce();
+
+//"Wire.read()<<8 | Wire.read();" means two registers are read and stored in the same variable
+  // acc_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
+  // acc_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x3D (ACCEL_YOUT_H) and 0x3E (ACCEL_YOUT_L)
+  // acc_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x3F (ACCEL_ZOUT_H) and 0x40 (ACCEL_ZOUT_L)
+  // //temperature.data = Wire.read()<<8 | Wire.read(); // reading registers: 0x41 (TEMP_OUT_H) and 0x42 (TEMP_OUT_L)
+  // gyro_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x43 (GYRO_XOUT_H) and 0x44 (GYRO_XOUT_L)
+  // gyro_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x45 (GYRO_YOUT_H) and 0x46 (GYRO_YOUT_L)
+  // gyro_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x47 (GYRO_ZOUT_H) and 0x48 (GYRO_ZOUT_L)
+  
+  // print out data
+  // Serial.print("aX = "); Serial.print(convert_int16_to_str(acc_x));
+
+  // Serial.print(" | aY = "); Serial.print(convert_int16_to_str(acc_y));
+  // Serial.print(" | aZ = "); Serial.print(convert_int16_to_str(acc_z));
+  // // the following equation was taken from the documentation [MPU-6000/MPU-6050 Register Map and Description, p.30]
+  // Serial.print(" | tmp = "); Serial.print(temperature/340.00+36.53);
+  // Serial.print(" | gX = "); Serial.print(convert_int16_to_str(gyro_x));
+  // Serial.print(" | gY = "); Serial.print(convert_int16_to_str(gyro_y));
+  // Serial.print(" | gZ = "); Serial.print(convert_int16_to_str(gyro_z));
+  // Serial.println();
+  //Serial.println(IR1.data);
+  
+//packing up the accelerometer data in ROS msgs
+
+  // imu.twist.linear.x = acc_x;
+  //  imu.twist.linear.x = acc_y;
+  //   imu.twist.linear.x = acc_z;
+  //    imu.twist.angular.x = gyro_x;
+  //    imu.twist.angular.y = gyro_y;
+  //    imu.twist.angular.z = gyro_z;
+  // imu.linear.x = acc_x;
+  //  imu.linear.x = acc_y;
+  //   imu.linear.x = acc_z;
+  //    imu.angular.x = gyro_x;
+  //    imu.angular.y = gyro_y;
+  //    imu.angular.z = gyro_z;
+
+
+//imu_pub.publish( &imu);
+
+
+
+  // delay
+  delay(10);
+  counter +1;
+//servo1.write(map(acc_x, -25000, 25000, 20, 150));
+
+
+  //nh.spinOnce();
+}
+
+
+//this function is a common way to map floats in arduino
+// float mapFloat(float input, float min, float max, float tomin, float tomax ){
+//    float output;
+    
+//      output = (input - min) * (tomax - tomin) / (max - min) + tomin;
+//      //I understand this as y = mx + b
+//      //m is the ratio of ranges (tomax - tomin)/(max - min)
+//      //x is the input minus it's y intercept
+//      //b is the y intercept of the desired range
+
+//      return output;
+      
+
+
+// }
+
+
+//  float mapFloat(float input, std_msgs::Float64 min, std_msgs::Float64 max, std_msgs::Float64 tomin, std_msgs::Float64 tomax ){
+//     float output;
+
+//     output = (input - min) * (tomax - tomin) / (max - min) + tomin;
+
+//     return output;
+      
+
+
+// }
+
+
+
+
+
+
+
+
+
+//if (Serial.available() > 0){
+//
+//  Serial.print(char(Serial.read()));
+//  
+//  if(Serial.available() == 0){
+//    Serial.println("");
+//    Serial.println("STRING READ SUCCESSFULL");
+//  }
+//  
+//}
+//
+//if(Serial2.available() > 0){
+//  Serial.println(Serial2.read());
+//   if(Serial2.available() == 0){
+//    Serial.println("");
+//    Serial.println("STRING READ SUCCESSFULL");
+//  }
+//}
+
+
+
+
+
