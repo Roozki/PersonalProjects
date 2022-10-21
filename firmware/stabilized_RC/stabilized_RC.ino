29729e292c53cb2c3207273d6596f821cf25d3c5
Roozki <zawadzkirowan@gmail.com>
2022-10-21 14:11:39 -0700

    manual control working, attempting accel feedback loop

--- firmware/stabilized_RC/stabilized_RC.ino
+++ firmware/stabilized_RC/stabilized_RC.ino
@@ -1,8 +1,3 @@
-
-
-
-
-
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
@@ -11,17 +6,17 @@
 #include <AccelStepper.h>
 
 
-#define throttlePin 2
-#define aileronPin 3
+#define throttlePin 3
+#define aileronPin 2
 #define elevatorPin 4
 
-#define prop1pin 5
-#define prop2pin 6
+#define prop1pin 6
+#define prop2pin 9
 
 Adafruit_MPU6050 mpu;
 
-Servo prop1; 
-Servo prop2; 
+Servo prop1;  //cw
+Servo prop2;  //ccw
 
 int counter;
 
@@ -60,15 +55,13 @@ int elevator = 0;
 int throttle1 = 0;
 int throttle2 = 0;
 
-int throttle1min = 90;
-int throttle2min = 90;
+//int throttle1min = 90;
+//int throttle2min = 90;
 
+int aileronAdj = 0;
 
-
-const int imu_address = 0x68; //I2C address of imu
+const int imu_address = 0x68;  //I2C address of imu
 //accelerometer
-
-
 
 float acc_x;
 float acc_y;
@@ -78,37 +71,19 @@ float gyro_y;
 float gyro_z;
 int temperature;
 
+int mot1 = 7;
+int mot2 = 8;
+int motpwm = 5;
 
 
+int elevatorSlowdownCount = 0;
 
-
-
-//motor control
-
-//AccelStepper wheel;
-//
-// void driveMotor(int motor, int dir, int speed){
-// int tmp = speed / 2;
-//   digitalWrite(motorDirPin[motor], dir);
-
-// if(micros() % speed == 0){
-//  if(mircos() % tmp == 0){
-//   digitalWrite(motorPulPin[motor], HIGH);
-//  }else{
-//      digitalWrite(motorPulPin[motor], LOW);
-
-//  }
-// }
-// }
-int mot1 = 8;
-int mot2 = 9;
-int motpwm = 10;
 
 void setup() {
 
-Serial.begin(115200);
- while (!Serial) {
-    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+  Serial.begin(115200);
+  while (!Serial) {
+    delay(10);  // will pause Zero, Leonardo, etc until serial console opens
   }
 
   if (!mpu.begin()) {
@@ -117,52 +92,60 @@ Serial.begin(115200);
       delay(10);
     }
   }
-pinMode(mot1, OUTPUT);
-pinMode(mot2, OUTPUT);
-pinMode(motpwm, OUTPUT);
+  pinMode(mot1, OUTPUT);
+  pinMode(mot2, OUTPUT);
+  pinMode(motpwm, OUTPUT);
 
-  
-  
+
+
   pinMode(throttlePin, INPUT_PULLUP);
-   pinMode(aileronPin, INPUT_PULLUP);
-    pinMode(elevatorPin, INPUT_PULLUP);
- attachInterrupt(digitalPinToInterrupt(throttlePin), throttleCHECK, CHANGE);
- attachInterrupt(digitalPinToInterrupt(aileronPin), aileronCHECK, CHANGE);
- attachInterrupt(digitalPinToInterrupt(elevatorPin), elevatorCHECK, CHANGE);
+  pinMode(aileronPin, INPUT_PULLUP);
+  pinMode(elevatorPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(throttlePin), throttleCHECK, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(aileronPin), aileronCHECK, CHANGE);
+  //attachInterrupt(digitalPinToInterrupt(elevatorPin), elevatorCHECK, CHANGE);
 
-mpu.setAccelerometerRange(MPU6050_RANGE_16_G);
+  mpu.setAccelerometerRange(MPU6050_RANGE_16_G);
   mpu.setGyroRange(MPU6050_RANGE_250_DEG);
   mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
   Serial.println("mpu initiation appears successfull");
   delay(100);
 
-// Wire.begin();
-//   Wire.beginTransmission(imu_address);
-//   Wire.write(0x6B); //PWR_MGMT_1 register
-//   Wire.write(0); //wakey wakey MPU-6050
-//   Wire.endTransmission(true);
-//  prop1.attach(prop1pin);
-//  prop2.attach(prop2pin);
-   
-
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+  // Wire.begin();
+  //   Wire.beginTransmission(imu_address);
+  //   Wire.write(0x6B); //PWR_MGMT_1 register
+  //   Wire.write(0); //wakey wakey MPU-6050
+  //   Wire.endTransmission(true);
+  //  prop1.attach(prop1pin);
+  //  prop2.attach(prop2pin);
+  prop1.attach(prop1pin);
+  prop2.attach(prop2pin);
 }
 
 
-void loop() { 
- forward();
+void loop() {
+  elevatorSlowdownCount++;
 
- analogWrite(motpwm, map(throttle, 1100, 1900, 0, 255));
-sensors_event_t a, g, temp;
+  //wheel control
+  if (elevator > 1600) {
+    forward();
+
+    analogWrite(motpwm, map(elevator, 1600, 1900, 0, 255));
+  } else if (elevator < 1400) {
+    backward();
+    analogWrite(motpwm, map(elevator, 1400, 1100, 0, 255));
+
+  } else {
+    stop();
+  }
+
+
+
+
+
+  sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
-//delay(600);
-//stop();
-//delay(600);
-//backward();
-//delay(600);
-//stop();
-//delay(600);
-
+/*
  Serial.print("AccelX:");
   Serial.print(a.acceleration.x);
   Serial.print(",");
@@ -182,23 +165,62 @@ sensors_event_t a, g, temp;
   Serial.print(g.gyro.z);
   Serial.println("");
 
-if (throttletmp < 2000){
-  throttle = throttletmp;
-}
-if (ailerontmp < 2000){
-  aileron = ailerontmp;
-}
-if (elevatortmp < 2000){
-  elevator = elevatortmp;
-}
-  //Serial.print("throttle:         ");   Serial.print(throttle);   Serial.print("     aileron:   ");   Serial.print(aileron);   Serial.print("     elevator:  ");   Serial.println(elevator);
+*/
 
 
-throttle1 = throttle;
-throttle2 = throttle;
+  if (throttletmp < 2000) {
+    throttle = throttletmp;
+  }
+  if (ailerontmp < 2000) {
+    aileron = ailerontmp;
+  }
 
-//analogWrit
+  // if (elevatortmp < 2000){
+  //   elevator = elevatortmp;
+  // }
 
+  if (elevatorSlowdownCount > 20) {
+    elevator = pulseIn(elevatorPin, HIGH);
+    elevatorSlowdownCount = 0;
+  }
+
+
+  // Serial.print("throttle:         ");
+  // Serial.print(throttle);
+  // Serial.print("     aileron:   ");
+  // Serial.print(aileron);
+  // Serial.print("     elevator:  ");
+  // Serial.println(elevator);
+
+
+  throttle1 =  map(throttle, 1100, 2000, 0, 900);
+  throttle2 = map(throttle, 1100, 2000, 0, 900);;
+//   throttle1 = map(throttle1, 1200, 1900, 95, 103);
+// throttle2 = map(throttle2, 1200, 1900, 95, 101);
+
+aileronAdj = map(aileron, 1100, 2000, 70, -70);
+
+
+
+throttle1 = map(throttle1, 0, 900, 1500, 2000);
+throttle2 = map(throttle2, 0, 900, 1500, 2000);
+
+throttle1 += (aileronAdj + 20);
+
+throttle2 -= aileronAdj;
+  //analogWrit
+
+// Serial.print(throttle1);
+// Serial.print("   ");
+// Serial.println(throttle2);
+  //manual lift
+  //try with servo
+  //prop1.write(throttle1); 
+  //prop2.write(throttle2);
+  //analogwrite got weird calibration
+  //do pwm with stuff that makes sense
+  prop1.writeMicroseconds(throttle1);
+  prop2.writeMicroseconds(throttle2);
 
 
 
@@ -209,91 +231,82 @@ throttle2 = throttle;
 
 
   Wire.beginTransmission(imu_address);
-  Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H) [MPU-6000 and MPU-6050 Register Map and Descriptions Revision 4.2, p.40]
- Wire.endTransmission(false); // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
-  Wire.requestFrom(imu_address, 7*2, true); // request a total of 7*2=14 registers
-  
+  Wire.write(0x3B);                            // starting with register 0x3B (ACCEL_XOUT_H) [MPU-6000 and MPU-6050 Register Map and Descriptions Revision 4.2, p.40]
+  Wire.endTransmission(false);                 // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
+  Wire.requestFrom(imu_address, 7 * 2, true);  // request a total of 7*2=14 registers
 
-//"Wire.read()<<8 | Wire.read();" means two registers are read and stored in the same variable
-  acc_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
-  acc_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x3D (ACCEL_YOUT_H) and 0x3E (ACCEL_YOUT_L)
-  acc_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x3F (ACCEL_ZOUT_H) and 0x40 (ACCEL_ZOUT_L)
-  temperature = Wire.read()<<8 | Wire.read(); // reading registers: 0x41 (TEMP_OUT_H) and 0x42 (TEMP_OUT_L)
-  gyro_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x43 (GYRO_XOUT_H) and 0x44 (GYRO_XOUT_L)
-  gyro_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x45 (GYRO_YOUT_H) and 0x46 (GYRO_YOUT_L)
-  gyro_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x47 (GYRO_ZOUT_H) and 0x48 (GYRO_ZOUT_L)
-  
+
+  //"Wire.read()<<8 | Wire.read();" means two registers are read and stored in the same variable
+  acc_x = Wire.read() << 8 | Wire.read();        // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
+  acc_y = Wire.read() << 8 | Wire.read();        // reading registers: 0x3D (ACCEL_YOUT_H) and 0x3E (ACCEL_YOUT_L)
+  acc_z = Wire.read() << 8 | Wire.read();        // reading registers: 0x3F (ACCEL_ZOUT_H) and 0x40 (ACCEL_ZOUT_L)
+  temperature = Wire.read() << 8 | Wire.read();  // reading registers: 0x41 (TEMP_OUT_H) and 0x42 (TEMP_OUT_L)
+  gyro_x = Wire.read() << 8 | Wire.read();       // reading registers: 0x43 (GYRO_XOUT_H) and 0x44 (GYRO_XOUT_L)
+  gyro_y = Wire.read() << 8 | Wire.read();       // reading registers: 0x45 (GYRO_YOUT_H) and 0x46 (GYRO_YOUT_L)
+  gyro_z = Wire.read() << 8 | Wire.read();       // reading registers: 0x47 (GYRO_ZOUT_H) and 0x48 (GYRO_ZOUT_L)
+
   //print out data
-  Serial.print("aX = "); Serial.print(String(acc_x));
+  // Serial.print("aX = "); Serial.print(String(acc_x));
 
-  Serial.print(" | aY = "); Serial.print(String(acc_y));
-  Serial.print(" | aZ = "); Serial.print(String(acc_z));
-  // the following equation was taken from the documentation [MPU-6000/MPU-6050 Register Map and Description, p.30]
-  //Serial.print(" | tmp = "); Serial.print(temperature/340.00+36.53);
-  Serial.print(" | gX = "); Serial.print(String(gyro_x));
-  Serial.print(" | gY = "); Serial.print(String(gyro_y));
-  Serial.print(" | gZ = "); Serial.print(String(gyro_z));
-   Serial.println();
+  // Serial.print(" | aY = "); Serial.print(String(acc_y));
+  // Serial.print(" | aZ = "); Serial.print(String(acc_z));
+  // // the following equation was taken from the documentation [MPU-6000/MPU-6050 Register Map and Description, p.30]
+  // //Serial.print(" | tmp = "); Serial.print(temperature/340.00+36.53);
+  // Serial.print(" | gX = "); Serial.print(String(gyro_x));
+  // Serial.print(" | gY = "); Serial.print(String(gyro_y));
+  // Serial.print(" | gZ = "); Serial.print(String(gyro_z));
+  //  Serial.println();
   //Serial.println(IR1.data);
 
 
 
 
-  
-//servo1.write(map(acc_x, -25000, 25000, 20, 150));
 
-
+  //servo1.write(map(acc_x, -25000, 25000, 20, 150));
 }
 
-void throttleCHECK(){
-throttlecurrent = micros();
-if (throttlecurrent > throttleprev){
-  throttletmp = throttlecurrent - throttleprev;
-  throttleprev = throttlecurrent;
-  
+void throttleCHECK() {
+  throttlecurrent = micros();
+  if (throttlecurrent > throttleprev) {
+    throttletmp = throttlecurrent - throttleprev;
+    throttleprev = throttlecurrent;
+  }
 }
 
+void aileronCHECK() {
 
-}
-
-void aileronCHECK(){
- 
   aileroncurrent = micros();
-if (aileroncurrent > aileronprev){
-  ailerontmp = aileroncurrent - aileronprev;
-  aileronprev = aileroncurrent;
-  
+  if (aileroncurrent > aileronprev) {
+    ailerontmp = aileroncurrent - aileronprev;
+    aileronprev = aileroncurrent;
+  }
 }
 
-}
+void elevatorCHECK() {
 
-void elevatorCHECK(){
- 
   elevatorcurrent = micros();
-if (elevatorcurrent > elevatorprev){
-  elevatortmp = elevatorcurrent - elevatorprev;
-  elevatorprev = elevatorcurrent;
-  
+  if (elevatorcurrent > elevatorprev) {
+    elevatortmp = elevatorcurrent - elevatorprev;
+    elevatorprev = elevatorcurrent;
+  }
 }
 
+void forward() {
+  digitalWrite(mot1, LOW);
+  digitalWrite(mot2, HIGH);
+  //analogWrite(motpwm, 100);
 }
 
-void forward(){
-digitalWrite(mot1, LOW);  
-digitalWrite(mot2, HIGH);  
-//analogWrite(motpwm, 100);
-}
-
-void backward(){
-  digitalWrite(mot1, HIGH);  
-  digitalWrite(mot2, LOW);  
+void backward() {
+  digitalWrite(mot1, HIGH);
+  digitalWrite(mot2, LOW);
   //analogWrite(motpwm, 200);
 }
 
-void stop(){
-  digitalWrite(mot1, LOW);  
-  digitalWrite(mot2, LOW);  
-  analogWrite(motpwm, 0);
+void stop() {
+  digitalWrite(mot1, LOW);
+  digitalWrite(mot2, LOW);
+  //analogWrite(motpwm, 0);
 }
 
 
@@ -320,12 +333,12 @@ void stop(){
 //if (Serial.available() > 0){
 //
 //  Serial.print(char(Serial.read()));
-//  
+//
 //  if(Serial.available() == 0){
 //    Serial.println("");
 //    Serial.println("String READ SUCCESSFULL");
 //  }
-//  
+//
 //}
 //
 //if(Serial2.available() > 0){
@@ -335,8 +348,3 @@ void stop(){
 //    Serial.println("String READ SUCCESSFULL");
 //  }
 //}
-
-
-
-
-
