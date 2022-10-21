5d43004e4f3851276068034a66a357430b0be498
Roozki <zawadzkirowan@gmail.com>
2022-10-21 14:27:36 -0700

    ignoring

--- build/microcontroller_comms/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rowan/PersonalProjectsWS/src/microcontroller_comms
-# Build directory: /home/rowan/PersonalProjectsWS/build/microcontroller_comms
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
