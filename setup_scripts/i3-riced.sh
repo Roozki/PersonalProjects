d854bccb4374d31b3de7803a87f45132c7a540f4
Roozki <zawadzkirowan@gmail.com>
2024-01-29 08:14:02 -0800

    setup script edits

--- setup_scripts/i3-riced.sh
+++ setup_scripts/i3-riced.sh
@@ -1,6 +1,8 @@
+cd ~/
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install i3 -y
 sudo apt install i3-blocks -y
 sudo apt install picom -y
-
+sudo apt install kitty -y
+sudo apt install i3lock i3lock-fancy -y
